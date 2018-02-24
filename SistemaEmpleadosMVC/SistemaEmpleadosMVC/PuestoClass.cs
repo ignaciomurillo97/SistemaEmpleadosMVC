685a6d62498fd0af6869d2a4022856afdc43b1d4
ignaciomurillo97 <ignaciomurillo97@gmail.com>
2018-02-23 21:40:47 -0600

    Puesto Class agregado

---
+++ SistemaEmpleadosMVC/SistemaEmpleadosMVC/PuestoClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEmpleados
+{
+    class PuestoClass
+    {
+        public int IdPuesto { get; set; }
+        public string Nombre_Puesto { get; set; }
+        public decimal Salario_Pagado { get; set; }
+    }
+}
